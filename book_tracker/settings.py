c0afe277f08e23a556927035ed77e0c9f4896b0a
Radu Iordache <radu_iordache_83@yahoo.com>
2022-05-17 21:46:53 +0300

    first book_app

--- book_tracker/settings.py
+++ book_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_qu!)vzz09_!yx)218y2!d@n4c%74&uvlu4!&iv8_l0l!e9n(0'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
